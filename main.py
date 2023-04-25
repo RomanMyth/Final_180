e9b53a1d8aed2f31794f62dbcffb46ec5c56d6b7
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-04-25 14:49:45 -0400

    password gitignore

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
-
+import Conn
 app = Flask(__name__)
-conn_str = "mysql://root:Axwell99!@localhost/BoatDB"
+conn_str = f"mysql://root:{Conn.password()}@localhost/DbProject"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('')
+
+if __name__ == '__main__':
+    app.run(debug=True)
