0ded9facfe49828605e0f540c2d8a0c89cfeb29f
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-04-26 15:13:01 -0400

    auth fixes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
@@ -8,12 +10,19 @@ conn_str = f"mysql://root:{Conn.password()}@localhost/DbProject"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
+<<<<<<< HEAD
 
-@app.route('/', method=['POST'])
+=======
+@app.route('/', methods=['GET'])
+def get_signup():
+    return render_template('Base.html')
+
+@app.route('/', methods=['POST'])
+>>>>>>> bbe8275 (auth fixes)
 def signup():
     try:
         conn.execute(
-            text("INSERT INTO UserInfo (`Username`, `Name`, `Email`, `Password`, `Account_Type`) values (:Username, :Password, 'Customer')"),
+            text("INSERT INTO UserInfo (`Username`, `Name`, `Email`, `Password`, `Account_Type`) values (:Username, :Password, :Username, :Password, 'Customer')"),
             request.form
         )
         conn.commit()
@@ -24,22 +33,31 @@ def signup():
         print(error)
         return render_template('Base.html', error=error, success=None)
 
+<<<<<<< HEAD
+=======
+@app.route('/login', methods=['GET'])
+def get_login():
+    return render_template('login.html')
 
-@app.route('/login', method=['POST'])
+@app.route('/login', methods=['POST'])
+>>>>>>> bbe8275 (auth fixes)
 def login():
     try:
-        conn.execute(
+        auth = conn.execute(
             text("SELECT if(Password = :Password, 'Yes', 'No') FROM UserInfo WHERE Username = :Info OR Email = :Info;"),
             request.form
-        )
-        if conn == 'Yes':
+        ).one_or_none()
+        print(auth)
+        print(auth)
+        if auth[0] == 'Yes':
             return render_template('Products.html', error=None, success="Data inserted successfully!")
         else:
+            print(auth)
             return render_template('login.html', error=None, success="Incorrect Username/Email or Password")
     except Exception as e:
         error = e.orig.args[1]
         print(error)
-        return render_template('login.html', error=error, success=None)
+        return render_template('login.html', error=error, success=f'hello')
 
 
 if __name__ == '__main__':
