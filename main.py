58f359cb89768f8e7c8638916663deebfc625df8
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-04-26 13:28:25 -0400

    login authorization basics

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
+
+
 import Conn
 app = Flask(__name__)
 conn_str = f"mysql://root:{Conn.password()}@localhost/DbProject"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
-@app.route('/', methodd=['POST'])
+@app.route('/', method=['POST'])
 def signup():
     try:
         conn.execute(
@@ -20,6 +22,23 @@ def signup():
         error = e.orig.args[1]
         print(error)
         return render_template('Base.html', error=error, success=None)
+
+@app.route('/login', method=['POST'])
+def login():
+    try:
+        conn.execute(
+            text("SELECT if(Password = :Password, 'Yes', 'No') FROM UserInfo WHERE Username = :Info OR Email = :Info;"),
+            request.form
+        )
+        if conn == 'Yes':
+            return render_template('Products.html', error=None, success="Data inserted successfully!")
+        else:
+            return render_template('login.html', error=None, success="Incorrect Username/Email or Password")
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('login.html', error=error, success=None)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
