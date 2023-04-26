d860a489bd9a08a6952415d2a3cea9ff96ce8baf
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-04-26 12:16:55 -0400

    basic post structure

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ conn_str = f"mysql://root:{Conn.password()}@localhost/DbProject"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
-@app.route('/')
-def index():
-    return render_template('')
+@app.route('/', methodd=['POST'])
+def signup():
+    try:
+        conn.execute(
+            text("INSERT INTO UserInfo (`Username`, `Name`, `Email`, `Password`, `Account_Type`) values (:Username, :Password, 'Customer')"),
+            request.form
+        )
+        conn.commit()
+        return render_template('sign.html', error=None, success="Data inserted successfully!")
+
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('Base.html', error=error, success=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
