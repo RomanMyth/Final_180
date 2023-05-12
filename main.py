19b0db2442976019fb13ca69014f7f6aa6f60528
RomanMyth <117763464+RomanMyth@users.noreply.github.com>
2023-05-12 10:51:33 -0400

    search products and cart for new users

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def signup():
             text("SELECT User_id FROM UserInfo WHERE Username = :Username"),
             request.form
         ).one_or_none()
-        print(user)
         conn.execute(
             text(f"INSERT INTO CART (`User_id`) SELECT User_id FROM UserInfo WHERE Username = :Username"),
             request.form
@@ -38,7 +37,6 @@ def signup():
 
     except Exception as e:
         error = e.orig.args[1]
-        print(error)
         return render_template('Base.html', error=error, success=None)
 
 
@@ -49,39 +47,43 @@ def get_login():
 
 @app.route('/login', methods=['POST'])
 def login():
-    try:
-        auth = conn.execute(
-            text("SELECT if(Password = :Password, 'Yes', 'No'), Account_Type, User_id FROM UserInfo WHERE Username = :Info OR Email = :Info;"),
-            request.form
-        ).one_or_none()
-        print(auth)
-        if auth is not None and auth[0] == 'Yes':
-            if auth[1] == 'Customer':
-                cookie = redirect('/products')
-                cookie.set_cookie('User_id', str(auth[2]))
-                return cookie
-            else:
-                cookie = redirect('/product_edit')
-                cookie.set_cookie('User_id', str(auth[2]))
-                return cookie
+    auth = conn.execute(
+        text("SELECT if(Password = :Password, 'Yes', 'No'), Account_Type, User_id FROM UserInfo WHERE Username = :Info OR Email = :Info;"),
+        request.form
+    ).one_or_none()
+    if auth is not None and auth[0] == 'Yes':
+        if auth[1] == 'Customer':
+            cookie = redirect('/products')
+            cookie.set_cookie('User_id', str(auth[2]))
+            conn.execute(text(f"INSERT IGNORE INTO CART (`User_id`) VALUES ({auth[2]})"))
+            conn.commit()
+            return cookie
         else:
-            return render_template('login.html', error=None, success="Incorrect Username/Email or Password")
-
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
-        return render_template('login.html', error=error, success=f'hello')
+            cookie = redirect('/product_edit')
+            cookie.set_cookie('User_id', str(auth[2]))
+            return cookie
+    else:
+        return render_template('login.html', error=None, success="Incorrect Username/Email or Password")
 
 
 @app.route('/', methods=['GET'])
 def default():
     products = conn.execute(text(f"SELECT * FROM Products Natural JOIN Images Left JOIN (SELECT * FROM Discount WHERE Expiration IS NULL or now() < Expiration) as vdiscount ON Products.Product_id = vdiscount.Product_id;")).all()
+
     return render_template('Products_start.html', products=products, success=None)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def get_searched_products():
+    param = request.form['Search']
+    products = conn.execute(text(f"SELECT * FROM Products Natural JOIN Images Left JOIN (SELECT * FROM Discount WHERE Expiration IS NULL or now() < Expiration) as vdiscount ON Products.Product_id = vdiscount.Product_id WHERE Products.Product_name LIKE '%{param}%' OR Products.Description LIKE '%{param}%';"), request.form).all()
+
+    return render_template('Products.html', products=products, success=None)
 
 @app.route('/products', methods=['GET'])
 def get_products():
     products = conn.execute(text(f"SELECT * FROM Products Natural JOIN Images Left JOIN (SELECT * FROM Discount WHERE Expiration IS NULL or now() < Expiration) as vdiscount ON Products.Product_id = vdiscount.Product_id;")).all()
-    print(products)
+
     return render_template('Products.html', products=products, success=None)
 
 
@@ -89,7 +91,6 @@ def get_products():
 def add_to_cart():
     id = request.cookies.get('User_id')
     cid = conn.execute(text(f"SELECT Cart_id FROM cart Where User_id = {id};")).one_or_none()
-    print(cid[0])
     conn.execute(
         text(f"INSERT INTO Cart_Items(`Cart_id`, `Product_id`, `Price`) VALUES ({cid[0]}, :Product_id, :Price);"),
         request.form
@@ -103,11 +104,11 @@ def add_to_cart():
 def get_vendor_products():
     id = request.cookies.get('User_id')
     account = conn.execute(text(f'SELECT Account_type FROM UserInfo WHERE User_id = {id}')).one_or_none()
-    print(account)
     if account[0] == 'Vendor':
         products = conn.execute(text(f"SELECT * FROM Products Where User_id = {id};")).all()
     else:
         products = conn.execute(text(f"SELECT * FROM Products")).all()
+
     return render_template('ProductEdit.html', products=products, success=None)
 
 
@@ -116,6 +117,7 @@ def update_products():
     column = request.form['Attribute']
     conn.execute(text(f'UPDATE Products SET {column} = :Change WHERE Product_id = :Product_id'), request.form)
     conn.commit()
+
     return redirect('/product_edit')
 
 
@@ -126,6 +128,7 @@ def delete_products():
     conn.execute(text(f'DELETE FROM Sizes Where Product_id = :Product_id'), request.form)
     conn.execute(text(f'DELETE FROM Products Where Product_id = :Product_id'), request.form)
     conn.commit()
+
     return redirect('/product_edit')
 
 
@@ -136,14 +139,13 @@ def add_products():
     vendors = []
     if account[0][0] == 'Admin':
         vendors = conn.execute(text(f'SELECT Account_Type, User_id, Name From UserInfo Where Account_Type = "Vendor"')).all()
+
     return render_template('ProductAdd.html', account=account, vendors=vendors, id=id)
 
 
 @app.route('/product_add', methods=['POST'])
 def add_products_post():
     warranty = request.form["warranty"]
-    print(warranty)
-    print(type(warranty))
     if warranty == "None":
         warranty = sqlalchemy.sql.null()
         conn.execute(
@@ -158,10 +160,7 @@ def add_products_post():
                 f"INSERT INTO Products (Product_name, Description, Quantity, Warranty, Price, User_id) Values (:productname, :description, :quantity, '{warranty}', :price, :vendorid)"),
             request.form
         )
-    print(warranty)
-    print(type(warranty))
     pid = conn.execute(text("SELECT max(Product_id) FROM Products;")).one_or_none()
-    print(pid[0])
     sizes = request.form["Sizes"]
     list_sizes = sizes.split()
     for size in list_sizes:
@@ -181,6 +180,7 @@ def add_products_post():
         request.form
     )
     conn.commit()
+
     return redirect("/product_add")
 
 
@@ -188,13 +188,12 @@ def add_products_post():
 def get_cart_items():
     id = request.cookies.get('User_id')
     items = conn.execute(text(f"SELECT c.User_id, c.Cart_id, ci.Product_id, p.Product_name, ci.Price FROM Cart c NATURAL JOIN Cart_items ci JOIN Products p WHERE c.Cart_id = (SELECT Cart_id FROM Cart WHERE User_id = {id} LIMIT 1) AND p.Product_id = ci.Product_id;")).all()
-    print(items)
-    print(len(items))
     if len(items) > 0:
         total = conn.execute(text(f"SELECT SUM(Price) FROM Cart_items WHERE Cart_id = {items[0][1]}")).one_or_none()
         total = round(total[0], 2)
     else:
         total = 0
+
     return render_template('Cart.html', items=items, total=total, success=None)
 
 
@@ -203,14 +202,13 @@ def checkout():
     id = request.cookies.get('User_id')
     now = datetime.now()
     now = now.strftime("%Y-%m-%d %H:%M:%S")
-    cart = request.form['Cart_id']
-    print(cart)
     conn.execute(text(f"INSERT INTO Orders (`Status`, `User_id`, `Cart_id`, `Date_ordered`) VALUES ('Pending', {id}, :Cart_id, '{now}');"), request.form)
     items = conn.execute(text(f"SELECT ci.Product_id FROM Cart_items ci JOIN Cart c WHERE c.Cart_id = ci.Cart_id and c.User_id = {id};")).all()
     for item in items:
         conn.execute(text(f"INSERT INTO Order_items (`Order_id`, `Product_id`) VALUES ((SELECT MAX(Order_id) FROM Orders), {item[0]});"))
     conn.execute(text(f"DELETE FROM Cart_items WHERE Cart_id = :Cart_id"), request.form)
     conn.commit()
+
     return redirect('/cart')
 
 
@@ -222,6 +220,7 @@ def remove_from_cart():
     cart = conn.execute(text(f'SELECT Cart_id from Cart WHERE User_id = {id}')).all()
     conn.execute(text(f'DELETE FROM Cart_items Where Cart_id = {cart[0][0]} and Product_id = :Product_id'), request.form)
     conn.commit()
+
     return redirect('/cart')
 
 
@@ -230,7 +229,6 @@ def remove_from_cart():
 def get_orders():
     User_id = request.cookies.get('User_id')
     orders = conn.execute(text(f'SELECT Status, date_ordered, Order_id FROM Orders WHERE User_id = {User_id}')).all()
-    print(orders)
     order_items = []
     products = []
     for i in orders:
@@ -241,10 +239,7 @@ def get_orders():
         for j in range(len(i)):
             items.append(conn.execute(text(f"SELECT * FROM Products Natural JOIN Images WHERE Products.Product_id = {i[j][0]};")).all())
         products.append(items)
-    print('\n', products)
 
-    print(order_items)
-    print(order_items[0])
     return render_template('order.html', orders=orders, products=products, success=None)
 
 
@@ -252,12 +247,10 @@ def get_orders():
 def get_admin_orders():
     id = request.cookies.get('User_id')
     user = conn.execute(text(f"SELECT Account_Type FROM UserInfo WHERE User_id = {id}")).one_or_none()
-    print(user)
     if user[0] == "Admin":
         orders = conn.execute(text(f'SELECT Status, date_ordered, Order_id FROM Orders')).all()
     else:
         orders = conn.execute(text(f'SELECT orders.Status, orders.date_ordered, orders.Order_id FROM Orders natural join order_items join products on products.product_id = order_items.product_id where products.user_id = {id};')).all()
-    print(orders)
     order_items = []
     products = []
     for i in orders:
@@ -269,16 +262,14 @@ def get_admin_orders():
             items.append(conn.execute(
                 text(f"SELECT * FROM Products Natural JOIN Images WHERE Products.Product_id = {i[j][0]};")).all())
         products.append(items)
-    print('\n', products)
 
-    print(order_items)
-    print(order_items[0])
     return render_template('Admin_Order.html', orders=orders, products=products, success=None)
 
 @app.route('/admin_order', methods=['POST'])
 def update_order_status():
     conn.execute(text(f'UPDATE Orders SET Status = :Status WHERE Order_id = :Order_id'), request.form)
     conn.commit()
+
     return redirect('/admin_order')
 
 
@@ -287,15 +278,11 @@ def get_customer_chat():
     id = request.cookies.get('User_id')
     chats = conn.execute(text(f'SELECT Chat_id, User_id2 FROM Chat Where User_id1 = {id}')).all()
     account = conn.execute(text(f'SELECT Account_type from UserInfo WHERE User_id = {id}')).all()
-    print(account)
     messages = []
     users = []
-    print(chats)
     for chat in range(len(chats)):
         messages.append(conn.execute(text(f'SELECT * FROM Chat_message WHERE Chat_id = {chats[chat][0]}')).all())
         users.append(conn.execute(text(f'SELECT Name FROM UserInfo WHERE User_id = {chats[chat][1]}')).all())
-    print(messages)
-    print(users)
 
     return render_template('Chat.html', chats=chats, messages=messages, id=id, users=users, account=account)
 
@@ -312,29 +299,24 @@ def get_admin_chat():
     id = request.cookies.get('User_id')
     chats = conn.execute(text(f'SELECT Chat_id, User_id1 FROM Chat Where User_id2 = {id}')).all()
     account = conn.execute(text(f'SELECT Account_type from UserInfo WHERE User_id = {id}')).all()
-    print(account)
     messages = []
     users = []
-    print(chats)
     for chat in range(len(chats)):
         messages.append(conn.execute(text(f'SELECT * FROM Chat_message WHERE Chat_id = {chats[chat][0]}')).all())
         users.append(conn.execute(text(f'SELECT Name FROM UserInfo WHERE User_id = {chats[chat][1]}')).all())
-    print(messages)
-    print(users)
 
     return render_template('Chat.html', chats=chats, messages=messages, id=id, users=users, account=account)
 
 
 @app.route('/reviews', methods=['GET', 'POST'])
 def get_reviews():
-
     list_reviews = conn.execute(text(f"SELECT * FROM Review Natural JOIN UserInfo WHERE Product_id = :id;"), request.form).all()
     name = conn.execute(text(f"SELECT Product_name FROM Products where Product_id = :id;"), request.form).one_or_none()
-    print(list_reviews)
     Success = True
     if len(list_reviews) <= 0:
         list_reviews = ["There are no reviews for this product yet."]
         Success = False
+
     return render_template('reviews.html', reviews=list_reviews, Success=Success, name=name)
 
 
@@ -350,6 +332,7 @@ def send_admin_message():
 def get_profile():
     id = request.cookies.get('User_id')
     profile = conn.execute(text(f"SELECT * FROM UserInfo where User_id = {id}")).one_or_none()
+
     return render_template('profile.html', profile=profile)
 
 
